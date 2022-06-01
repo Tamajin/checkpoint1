fe19591d72d9f6547b3170b76e3f989c663a0364
Tamajin <wild.jlq@gmail.com>
2022-06-01 22:51:56 +0200

    instruction JS 2 : create a button who invite user to give his/her name and change the default name with the userName given by prompt

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,3 +6,21 @@ const myAvatar = document.querySelector(".avatar");
 myAvatar.addEventListener("click",function(){
     myAvatar.src = "image/avatar-bis.png";
 });
+
+// instruction 2 : créer un bouton pour inviter l'utilisateur à renseigner son nom, puis insérer ce nom à la place de pseudo générique
+// je crée un bouton
+// je récupère et stocke la div dans la laquelle il va se situer
+// je crée un prompt au click sur le bouton puis un changement de texte et de couleur, le tout dans la même fonction addEventListener
+
+const btnName = document.createElement("button");
+const btnPosition = document.querySelector(".pink-bg");
+btnName.className = "btn-name";
+btnName.innerText = "Choose your Name"
+btnPosition.appendChild(btnName);
+btnName.addEventListener("click", function(event){
+    event.preventDefault();
+    const enterName = prompt("Enter your Name");
+    const changeName = document.querySelector("#name");
+    changeName.innerHTML = enterName;
+    changeName.style.color = "white";
+});
